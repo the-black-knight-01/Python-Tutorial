cba36b9e2df1ca6b5216ee4c2b4c3cbd34752d04
Rahul Patel <rahulpatel11315@gmail.com>
2018-12-06 19:01:25 +0530

    weka
    weka

---
+++ Comments in Python.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+print (text)
---
+++ Numbers in Python.py
@@ -0,0 +1,4 @@
+a,b = 0,1
+while(a<10):
+    print(a)
+    a,b=b,a+b
